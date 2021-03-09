83c129fb55ba5e951bdf627eec5b97e682a50f6a
Melih Melik SÖNMEZ <5006654+themelic@users.noreply.github.com>
2021-03-09 10:24:56 +0000

    Program.cs
    The main entry point for the application.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Samer_Import_Export
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
